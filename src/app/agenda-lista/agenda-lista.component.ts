ae730ed7b171d7bc3a4c88324de4cb3d47705468
carlosjc31 <carlosjc31@hotmail.com>
2024-11-27 17:54:31 -0300

    4º criando o serviço da agenda

--- src/app/agenda-lista/agenda-lista.component.ts
+++ src/app/agenda-lista/agenda-lista.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { Agenda } from '../agenda';
 
 @Component({
   selector: 'app-agenda-lista',
   standalone: false,
-
   templateUrl: './agenda-lista.component.html',
   styleUrl: './agenda-lista.component.css'
 })
 export class AgendaListaComponent {
-agendas: any;
+agendas: Agenda[] = [];
 
 }
---
+++ src/app/agenda.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Agenda } from './agenda';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AgendaService {
+  private url = 'http://localhost:3000/agendas';
+
+  constructor(private http: HttpClient) { }
+
+getAgendas(): Observable<Agenda[]> {
+  return this.http.get<Agenda[]>(this.url);
+}
+
+}
