1b045e6ac977db26828c1f4a1014fa3266ca5997
carlosjc31 <carlosjc31@hotmail.com>
2024-11-28 16:07:27 -0300

    12º finalizando home e navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgendaListaComponent } from './agenda-lista/agenda-lista.component';
 import { AgendaFormComponent } from './agenda-form/agenda-form.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'agendas', pathMatch: 'full' },
   { path: 'agendas', component: AgendaListaComponent },
-  { path: 'agendas/:id', component: AgendaFormComponent }
+  { path: 'agendas/:id', component: AgendaFormComponent },
+  { path: 'agenda', component: AgendaFormComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
